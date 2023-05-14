03af715a0c6c6bb9ab55133980f0e24b5fd08743
fotcorn <fotcorn@gmail.com>
2023-05-14 17:13:13 +0100

    fix struct_pointer_return sample

--- examples_c/struct_pointer_return.c
+++ examples_c/struct_pointer_return.c
@@ -6,15 +6,15 @@ struct MyStruct {
     int64_t b;
 };
 
-struct MyStruct* func() {
-    struct MyStruct s;
-    s.a = 3;
-    s.b = 5;
-    return &s;
+struct MyStruct* func(struct MyStruct *s) {
+    s->a = 3;
+    s->b = 5;
+    return s;
 }
 
 int main() {
-    struct MyStruct* s = func();
-    putcharln('0' + s->a);
-    putcharln('0' + s->b);
+    struct MyStruct s;
+    struct MyStruct* p = func(&s);
+    putcharln('0' + p->a);
+    putcharln('0' + p->b);
 }
