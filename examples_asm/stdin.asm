1992d14c732fdb582d8c6cd8f044a0af7de2e634
fotcorn <fotcorn@gmail.com>
2024-11-28 15:38:41 +0000

    remove stdin asm example, in instruction not implemented in llvm yet

--- examples_asm/stdin.asm
+++
@@ -1,8 +0,0 @@
-// not working right now, assembler does not know about the "in" instruction yet
-.globl _start
-_start:
-    loop:
-    in 1, %i0
-    out %i0, 1
-    jne %i0, 'q', _start
-    halt
